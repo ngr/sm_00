82e30fb29477db0400f45d723539019391ab3da0
Ubuntu <ubuntu@ip-10-232-65-219.us-west-2.compute.internal>
2015-06-11 13:32:25 +0000

    Alef 0.x - Addon 106

--- slave/serializers.py
+++ slave/serializers.py
@@ -48,14 +48,13 @@ class SlaveSerializer(serializers.ModelSerializer):
         return object.location.region.id
 
     def get_assignments(self, object):
-        """ Get running assignments. """
-        assignments = object.get_assignments(active=True).last()
-        return AssignmentSerializer(assignments, read_only=True).data if assignments else None
-
+        """ Get assignments of Slave. """
+        serializer = AssignmentSerializer(object.assignments.all().filter(date_released=None), many=True, read_only=True)
+        return serializer.data
+    
 class SlaveDetailSerializer(SlaveSerializer):
     """ A lot of attributes associated with the Slave. """
     
-    assignments  = serializers.SerializerMethodField(read_only=True)
     skills       = serializers.SerializerMethodField(read_only=True)
     intelligence = serializers.IntegerField(read_only=True)
     strength     = serializers.IntegerField(read_only=True)
@@ -67,11 +66,6 @@ class SlaveDetailSerializer(SlaveSerializer):
         fields = ('id', 'name', 'age', 'sex', 'race', 'exp', 'region_id', 'free', 'date_birth',\
             'intelligence', 'strength', 'agility', 'charisma', 'happiness', 'assignments', 'skills')
             
-    def get_assignments(self, object):
-        """ Get assignments of Slave. """
-        serializer = AssignmentSerializer(object.assignments.all().filter(date_released=None), many=True, read_only=True)
-        return serializer.data
-    
     def get_skills(self, object):
         """ Get serialized list of current Slave Skills. """
         serializer = SkillTrainedSerializer(object.get_skills().all(),  many=True, read_only=True)
